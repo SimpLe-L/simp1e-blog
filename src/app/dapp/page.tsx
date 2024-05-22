c9391a6cf664fd3ce9b931d5369d3285a6d1b53d
liaolei <liaolei@ygwjg.com>
2024-05-22 18:44:56 +0800

    chore: 0522 commit

--- src/app/dapp/page.tsx
+++ src/app/dapp/page.tsx
@@ -1,6 +1,14 @@
+import WalletConnector from "@/components/walletConnector"
+
+const config = {
+
+}
+
 const DappPage = () => {
   return (
-    <div className="w-full h-full">钱包</div>
+    <div className="w-full h-full p-[20px] flex items-center justify-center">
+      <WalletConnector config={config} />
+    </div>
   )
 }
 
--- src/components/walletConnector/index.tsx
+++ src/components/walletConnector/index.tsx
@@ -1,10 +1,36 @@
+"use client"
+
 import { IWallectConfig } from "@/types";
+import { useState } from "react";
 
 const WalletConnector: React.FC<IWallectConfig> = ({ config }) => {
+  const [visible, setVisible] = useState(false);
   return (
-    <div>
-      {config.text || "CONNECT"}
-    </div>
+    <>
+      <div
+        className="py-[4px] px-[14px] text-[--basic-text] rounded-xl border-2 boder-solid boder-[--secondry-text] cursor-pointer"
+        onClick={() => { setVisible(true) }}
+      >
+        {config.text || "CONNECT"}
+      </div>
+      {
+        visible
+          ?
+          <div className="fixed z-999 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-2xl flex flex-col gap-2">
+            <div className="flex">
+              <span className="">Ethereum network</span>
+              <div className="flex flex-wrap gap-1">
+              </div>
+            </div>
+            <div className="flex">
+              <span className="">BitCoin network</span>
+            </div>
+          </div>
+          :
+          null
+      }
+    </>
+
   )
 }
 
